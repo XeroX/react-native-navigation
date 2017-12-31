f41641a749929d6737857a140a8e89c8634f89f9
Nikolay Vznuzdaev <mrr.xerox@gmail.com>
2017-12-31 14:15:12 +0300

    Add native controller.

---
+++ lib/ios/ComponentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComponentViewController.h
+//  ReactNativeNavigation
+//
+//  Created by xerox on 12/30/17.
+//  Copyright © 2017 Wix. All rights reserved.
+//
+
+#ifndef ComponentViewController_h
+#define ComponentViewController_h
+
+
+#endif /* ComponentViewController_h */
---
+++ playground/ios/playground/NativeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NativeViewController.h
+//  playground
+//
+//  Created by xerox on 12/30/17.
+//  Copyright © 2017 Wix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NativeViewController : UIViewController
+
+@end
